7d7ffc3a611abddd749ff65c531875beab20a35a
Dasha_Ladatko <dasha.ladatko.14212@gmail.ru>
2016-11-12 00:54:31 +0200

    login (fail)

--- common/models/Faculty.php
+++ common/models/Faculty.php
@@ -52,10 +52,12 @@ class Faculty extends extActiveRecord
     public function rules()
     {
         return [
-            //['status', 'default', 'value' => 1],
-            ['status', 'default', 'value' => self::STATUS_ACTIVE],
+
+
             [['name'], 'required'],
-            [['status', 'created_at', 'created_by', 'updated_at', 'updated_by'], 'integer'],
+            ['status', 'default', 'value' => self::STATUS_ACTIVE],
+           // ['status', 'default', 'value' => 1],
+            [[ 'created_at', 'created_by', 'updated_at', 'updated_by'], 'integer'],
             [['name'], 'string', 'max' => 255],
         ];
     }
